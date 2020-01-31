101b392e62bacb02d90c20c2d7aad8c487ac2b89
Gloria Chen <cmxgloria@gmail.com>
2020-01-31 22:26:45 +1100

    udpated css

--- test.rb
+++ test.rb
@@ -3,6 +3,7 @@ require 'bcrypt'
 require 'pry'
 # update password in database, not let this on public area; next time run ruby test.rb in database to store in then delete before put in public
 conn = PG.connect(dbname: 'realestate_app')
+# only one account here, other will overwrite
 email = 'dt@ga.co'
 password = 'pudding'
 email = 'gloria@ga.co'
